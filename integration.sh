6f756a4cf42a4e233a737414d7e811956021f765
milesbardon <milesbardon@gmail.com>
2019-10-20 11:56:30 +0100

    Added input to integration

--- integration.sh
+++ integration.sh
@@ -1,7 +1,7 @@
 set -e
 
 RESULT=$(docker run -e CHROMIUM=/var/task/bin/headless-chromium -e CHROMEDRIVER=/var/task/bin/chromedriver -e AWS_LAMBDA_FUNCTION_MEMORY_SIZE=512 \
- --rm -v "$PWD"/package:/var/task lambci/lambda:python3.7 lambda_function.lambda_handler)
+ --rm -v "$PWD"/package:/var/task lambci/lambda:python3.7 lambda_function.lambda_handler '{"title": "test-title", "text": "test-text"}')
 
 if [[ $RESULT == *"card_file"* ]]; then
   exit 0
